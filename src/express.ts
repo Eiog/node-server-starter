0aa865a0c0550b4850d8722d9b50917c2fb161e7
antrioe <antrioe@gmali.com>
2024-01-04 16:14:39 +0800

    ci(deps): ci

---
+++ src/express.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import { createServer } from 'node:http'
+import express, { Router } from 'express'
+
+const PORT = process.env.PORT || 5631
+
+const app = express()
+app.use(express.json())
+const server = createServer(app)
+const router = Router()
+router.get('/', (req, res) => {
+  res.send('Express Is Started')
+})
+app.use(router)
+
+server.listen(PORT, () => {
+  console.log(`server is running at http://127.0.0.1:${PORT}/`)
+})
---
+++ src/h3.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+
+import { createServer } from 'node:http'
+import { createApp, createRouter, eventHandler, toNodeListener } from 'h3'
+
+const PORT = process.env.PORT || 5632
+
+const app = createApp()
+const server = createServer(toNodeListener(app))
+const router = createRouter()
+router.get('/', eventHandler(() => {
+  return 'H3 Is Started'
+}))
+app.use(router)
+
+server.listen(PORT, () => {
+  console.log(`server is running at http://127.0.0.1:${PORT}/`)
+})
