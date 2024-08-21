a0448d503341f9de3560c2f3e4d5fadf9d343391
antrioe <antrioe@gmali.com>
2024-08-21 12:00:29 +0800

    ci(deps): ci deps

--- api/_routes.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line ts/ban-ts-comment
-// @ts-nocheck
-import { Router } from 'express'
-import hello from './hello.ts'
-
-const router = Router()
-router.all('/hello', hello)
-export default router
--- scripts/vercel.dev.ts
+++ scripts/vercel.dev.ts
@@ -1,14 +1,33 @@
 import { createServer } from 'node:http'
 import process from 'node:process'
-import express from 'express'
-import routes from '../api/_routes'
+import { resolve } from 'node:path'
+
+import { readdirSync, statSync } from 'node:fs'
+import express, { Router } from 'express'
 
 const PORT = process.env.PORT || 5631
+const localPath = resolve(process.cwd(), 'api')
 
 const app = express()
 app.use(express.json())
-app.use('/api', routes)
+const router = Router()
 
+app.use('/api', router)
+const routesPath = readdirSync(localPath)
+  .filter(f => f.endsWith('.ts') && !f.startsWith('_') && statSync(`${localPath}/${f}`).isFile())
+const asyncImport = routesPath.map(async (m) => {
+  return {
+    path: `/${m.replace('.ts', '')}`,
+    fun: await import(`../api/${m}`),
+  }
+})
+Promise.all(asyncImport).then((asyncFunc) => {
+  asyncFunc.forEach((f) => {
+    const keys = Object.keys(f.fun)
+    if (keys.includes('default'))
+      router.all(f.path, f.fun.default)
+  })
+})
 const server = createServer(app)
 
 server.listen(PORT, () => {
